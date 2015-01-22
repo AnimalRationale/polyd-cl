690277ff3b4209bec4d34f29451992fa614bbe36
AnimalRationale <wojciech.mardyla@gmail.com>
2015-01-22 02:14:23 +0100

    Data input basic validation.

--- web/begin/codelab_form.dart
+++ web/begin/codelab_form.dart
@@ -11,5 +11,29 @@ class CodelabFormElement extends PolymerElement {
   int get maxTitleLength => Codelab.MAX_TITLE_LENGTH;
   int get maxDescriptionLength => Codelab.MAX_DESCRIPTION_LENGTH;
 
+  @observable String titleErrorMessage = '';
+  @observable String descriptionErrorMessage = '';
+
   CodelabFormElement.created() : super.created() {}
+
+  bool validateTitle() {
+    if (codelab.title.length < minTitleLength ||
+        codelab.title.length > maxTitleLength) {
+      titleErrorMessage = "Title must be between $minTitleLength and "
+          "$maxTitleLength characters.";
+      return false;
+    }
+    titleErrorMessage = '';
+    return true;
+  }
+
+  bool validateDescription() {
+    if (codelab.description.length > maxDescriptionLength) {
+      descriptionErrorMessage = "Description cannot be more than "
+          "$maxDescriptionLength characters.";
+      return false;
+    }
+    descriptionErrorMessage = '';
+    return true;
+  }
 }
