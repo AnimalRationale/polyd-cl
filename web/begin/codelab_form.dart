0341eddc6d2db21bb49fbccdaaa056bc94ae2a18
AnimalRationale <wojciech.mardyla@gmail.com>
2015-01-22 02:03:21 +0100

    Added basic logic for form.

--- web/begin/codelab_form.dart
+++ web/begin/codelab_form.dart
@@ -4,5 +4,12 @@ import 'dart:html' show CustomEvent, Event, Node;
 
 @CustomTag('codelab-form')
 class CodelabFormElement extends PolymerElement {
+  @published Codelab codelab;
+
+  List<String> get allLevels => Codelab.LEVELS;
+  int get minTitleLength =>  Codelab.MIN_TITLE_LENGTH;
+  int get maxTitleLength => Codelab.MAX_TITLE_LENGTH;
+  int get maxDescriptionLength => Codelab.MAX_DESCRIPTION_LENGTH;
+
   CodelabFormElement.created() : super.created() {}
 }
