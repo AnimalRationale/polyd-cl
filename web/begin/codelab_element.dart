94c684f9ace40a48bbd422f61ec4cf971610b125
AnimalRationale <wojciech.mardyla@gmail.com>
2015-01-23 19:41:18 +0100

    Init for editing entered events.

--- web/begin/codelab_element.dart
+++ web/begin/codelab_element.dart
@@ -4,6 +4,32 @@ import 'dart:html' show Event, Node, CustomEvent;
 
 @CustomTag('codelab-element')
 class CodelabElement extends PolymerElement {
+  @observable bool editing = false;
+    Codelab _cachedCodelab;
+    @published Codelab codelab;
   CodelabElement.created() : super.created() {}
-  @published Codelab codelab;
+
+  updateCodelab(Event e, var detail, Node sender) {
+    e.preventDefault();
+    editing = false;
+  }
+
+  startEditing(Event e, var detail, Node sender) {
+    e.preventDefault();
+    _cachedCodelab = new Codelab();
+    copyCodelab(codelab, _cachedCodelab);
+    editing = true;
+  }
+
+  cancelEditing(Event e, var detail, Node sender) {
+    e.preventDefault();
+    copyCodelab(_cachedCodelab, codelab);
+    editing = false;
+  }
+
+  copyCodelab(source, destination) {
+    destination.title = source.title;
+    destination.description = source.description;
+    destination.level = source.level;
+  }
 }
